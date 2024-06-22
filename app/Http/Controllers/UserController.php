4148a3fb7b752ad7759cf45c6ab17bf54b0ea87e
aldrichfcmw <aldrichmuktiwibiwo@gmail.com>
2024-06-22 15:30:23 +0700

    feat: create token to access api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,29 +4,42 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Spatie\Permission\Models\Role;
 
 class UserController extends Controller
 {
-    public function user(){
-        $title  = 'Dashboard';
-        $page   = 'User';
-        $subpage= 'List User';
+    // public function user()
+    // {
+    //     $title  = 'Dashboard';
+    //     $page   = 'List Users';
+    //     $data   =  User::get();
+    //     return view('dashboard.user', compact('title', 'page', 'data'));
+    // }
+
+    public function userList()
+    {
+        $title  = 'User';
+        $page   = 'List Users';
+        $subpage   = '';
         $data   =  User::get();
-        return view('dashboard.user',compact('title','page','subpage','data'));
-    }
-    
-    public function userAdd(){
-        $title  = 'Dashboard';
-        $page   = 'User';
-        $subpage= 'Add User';
-        return view('dashboard.user-add',compact('title','page','subpage'));
+        return view('dashboard.user-list', compact('title', 'page', 'subpage', 'data'));
     }
 
-    public function storeUser(Request $request){
+    public function userAdd()
+    {
+        $title  = 'User';
+        $page   = 'Add User';
+        $subpage   = '';
+        return view('dashboard.user-add', compact('title', 'page', 'subpage'));
+    }
+
+    public function storeUser(Request $request)
+    {
         // dd($request->all());
-        $validator = Validator::make($request->all(),[
+        $validator = Validator::make($request->all(), [
             'name'      => 'required|max:255|min:3',
             'username'  => 'required|unique:users,username|max:255',
             'email'     => 'required|email|unique:users,email',
@@ -38,25 +51,28 @@ class UserController extends Controller
             return redirect()->back()->withErrors($validator)->withInput();
         }
 
-        $data =[
+        $data = [
             'name'     => $request->name,
             'username'  => $request->username,
             'email'     => $request->email,
             'password'  => Hash::make($request->password),
         ];
         User::create($data);
-        return redirect()->route('user')->with('success','Akun berhasil dibuat');
+        return redirect()->route('user')->with('success', 'Akun berhasil dibuat');
     }
 
-    public function userEdit(Request $request,$uname){
-        $data = User::where('username',$uname)->first();
+    public function userEdit(Request $request, $uname)
+    {
+        $data = User::where('username', $uname)->first();
         // dd($data);
-        $title  = 'Dashboard';
-        $page   = 'Edit User';
-        return view('dashboard.user-edit',compact('title','page','data'));
+        $title  = 'Users';
+        $page   = 'Edit Users';
+        $subpage = '';
+        return view('dashboard.user-edit', compact('title', 'page', 'subpage', 'data'));
     }
 
-    public function userUpdate(Request $request,$uname){
+    public function userUpdate(Request $request, $uname)
+    {
         // $data = User::where('username',$uname)->first();
 
         $validator = Validator::make($request->all(), [
@@ -71,25 +87,71 @@ class UserController extends Controller
             return redirect()->back()->withErrors($validator)->withInput();
         }
         // dd($request->all());
-        $data =[
+        $data = [
             'name'     => $request->name,
             'username'  => $request->username,
             'email'     => $request->email,
         ];
-        if($request->password) $data['password']  = Hash::make($request->password);
+        if ($request->password) $data['password']  = Hash::make($request->password);
 
         // dd($data);
         User::whereUsername($uname)->update($data);
-        return redirect()->route('user')->with('success','Akun berhasil diubah');
-        
+        return redirect()->route('user')->with('success', 'Akun berhasil diubah');
     }
 
-    public function userDelete(Request $request,$uname){
+    public function userDelete(Request $request, $uname)
+    {
         $data = User::whereUsername($uname);
-        if($data){
-            $data -> delete();
+        if ($data) {
+            $data->delete();
         }
 
-        return redirect()->route('user')->with('success','Akun berhasil dihapus');
+        return redirect()->route('user')->with('success', 'Akun berhasil dihapus');
+    }
+
+    public function accountView()
+    {
+        $title  = 'User';
+        $page   = 'View';
+        $subpage   = 'Account';
+        $data = Auth::user();
+        $role = $data->roles->first()->name;
+        // dd($data);
+        return view('dashboard.account-view', compact('title', 'page', 'subpage', 'data', 'role'));
+    }
+
+    public function accountSecurity()
+    {
+        $title  = 'User';
+        $page   = 'View';
+        $subpage   = 'Security';
+        $data = Auth::user();
+        $role = $data->roles->first()->name;
+        // dd($data);
+        return view('dashboard.account-security', compact('title', 'page', 'subpage', 'data', 'role'));
+    }
+
+    public function accountToken()
+    {
+        $title  = 'User';
+        $page   = 'View';
+        $subpage   = 'Token';
+        $data = Auth::user();
+        $role = $data->roles->first()->name;
+        return view('dashboard.account-token', compact('title', 'page', 'subpage', 'data', 'role'));
+    }
+    public function accountTokenUpdate(Request $request)
+    {
+
+        $user = Auth::user();
+        $user->tokens()->delete();
+        $createToken = $user->createToken('api-token-' . $user->name)->plainTextToken;
+        $token = substr($createToken, 3); // Menghapus 3 karakter pertama
+        $data = [
+            'token_api'     => $token,
+        ];
+        User::whereUsername($user->username)->update($data);
+        return redirect()->route('account.token')->with('success', 'Success generate token');
+        // dd($token);
     }
 }
