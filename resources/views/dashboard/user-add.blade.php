c88d6f475d8467519c1ff153b9a5fcdc0650d072
aldrichfcmw <aldrichmuktiwibiwo@gmail.com>
2024-06-22 15:48:41 +0700

    feat: fix user issue

--- resources/views/dashboard/user-add.blade.php
+++ resources/views/dashboard/user-add.blade.php
@@ -5,7 +5,7 @@
 @extends('layouts.main')
 @section('content')
         <h4 class="py-3 mb-4">
-          <span class="text-muted fw-light">{{$title}}/{{$page}}/</span> {{$subpage}}
+          <span class="text-muted fw-light">{{$title}} / </span> {{$page}} 
         </h4>
           <!-- Default -->
           <div class="row">
