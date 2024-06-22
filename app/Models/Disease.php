d8b74287edd44935360e262dd23180e2637cfa3d
aldrichfcmw <aldrichmuktiwibiwo@gmail.com>
2024-06-22 15:34:34 +0700

    feat: change table stucture

--- app/Models/Disease.php
+++ app/Models/Disease.php
@@ -2,18 +2,40 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Disease extends Model
 {
+    use HasFactory;
 
-    protected $table = 'disease_table';
+    protected $table = 'table_disease';
 
 
     protected $fillable = [
-        'disease_id',
+        'disease_name',
         'latitude',
         'longitude',
+        'healthy_area',
+        'total_area',
+        'healthy_percentage',
+        'health_status',
         'image',
     ];
+
+    public static function generateName($prefix)
+    {
+        $lastRecord = self::where('disease_name', 'like', $prefix . '-%')
+            ->orderBy('disease_name', 'desc')
+            ->first();
+
+        if ($lastRecord) {
+            $lastNumber = (int) substr($lastRecord->disease_name, strlen($prefix) + 1);
+            $newNumber = $lastNumber + 1;
+        } else {
+            $newNumber = 1;
+        }
+
+        return $prefix . '-' . str_pad($newNumber, 3, '0', STR_PAD_LEFT);
+    }
 }
