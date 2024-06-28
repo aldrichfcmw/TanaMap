de2d4a4dfaebd034a11a66f55fd50295e00a1244
aldrichfcmw <aldrichmuktiwibiwo@gmail.com>
2024-06-28 15:39:26 +0700

    feat: add data weather table

--- database/migrations/2024_06_28_083322_create_weather_data_table.php
+++ database/migrations/2024_06_28_083322_create_weather_data_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('weather_data', function (Blueprint $table) {
+        Schema::create('table_weather_data', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('temp');
             $table->bigInteger('hum');
