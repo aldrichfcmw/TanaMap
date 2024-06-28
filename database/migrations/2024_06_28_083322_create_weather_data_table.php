639f7ae2a93e9b77c4c014efd4049cfb327776e7
aldrichfcmw <aldrichmuktiwibiwo@gmail.com>
2024-06-28 15:37:47 +0700

    feat: add data weather table

---
+++ database/migrations/2024_06_28_083322_create_weather_data_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('weather_data', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('temp');
+            $table->bigInteger('hum');
+            $table->double('press');
+            $table->bigInteger('uv');
+            $table->double('rainfall');
+            $table->double('windspeed');
+            $table->string('winddir');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('weather_data');
+    }
+};
